c4932e9afb4747c68ac0e770143293140fa45d93
Allen <liao4792856@gmail.com>
2023-12-25 19:11:21 +0800

    refactor move method

--- Original.src/CalculateMove.java
+++ Original.src/CalculateMove.java
@@ -9,55 +9,27 @@ public class CalculateMove {
 
     public ArrayList<String> move(
             ChessGameBoard board,
-            int numMoves, String direction ){
+            int numMoves, Direction d ){
         ArrayList<String> moves = new ArrayList<String>();
         int count = 0;
         if ( piece.isPieceOnScreen() ){
             for (int i = 1; i < 8 && count < numMoves; i++ ){
-                if (piece.isOnScreen( rowDirection(piece.getRow(),i,direction), colDirection(piece.getColumn(), i,direction ))){
-                    if ( board.getCell(  rowDirection(piece.getRow(),i,direction),colDirection(piece.getColumn(), i,direction )).getPieceOnSquare()
-                            == null){
-                        moves.add(rowDirection(piece.getRow(),i,direction) + "," + colDirection(piece.getColumn(), i,direction ));
-                        count++;
-                    } else if (piece.isEnemy( board, rowDirection(piece.getRow(),i,direction), colDirection(piece.getColumn(), i,direction ))) {
-                        moves.add(rowDirection(piece.getRow(),i,direction) + "," + colDirection(piece.getColumn(), i,direction ));
-                        count++;
-                        break;
-                    }else {
-                        break;
-                    }
+                if (!piece.isOnScreen( d.getRowMove(piece.getRow(),i), d.getColMove(piece.getColumn(), i))){
+                    break;
                 }
-                else {
+                if ( board.getCell(d.getRowMove(piece.getRow(),i),d.getColMove(piece.getColumn(), i)).getPieceOnSquare()
+                        == null){
+                    moves.add(d.getRowMove(piece.getRow(),i) + "," + d.getColMove(piece.getColumn(), i));
+                    count++;
+                } else if (piece.isEnemy( board, d.getRowMove(piece.getRow(),i), d.getColMove(piece.getColumn(), i))) {
+                    moves.add(d.getRowMove(piece.getRow(),i) + "," + d.getColMove(piece.getColumn(), i));
+                    count++;
+                    break;
+                }else {
                     break;
                 }
             }
         }
         return moves;
     }
-
-
-    public int rowDirection(int position,int i, String direction){
-        switch (direction){
-            case "southEast", "southWest", "south":
-                return position + i;
-            case "northEast", "northWest", "north":
-                return position - i;
-            case "west", "east":
-                return position;
-        }
-        return 0;
-    }
-
-    public int colDirection(int position,int i, String direction){
-        switch (direction){
-            case "southEast","northEast","east" :
-                return position + i;
-            case "southWest", "northWest","west" :
-                return position - i;
-            case "north", "south":
-                return position;
-        }
-        return 0;
-    }
-
 }
